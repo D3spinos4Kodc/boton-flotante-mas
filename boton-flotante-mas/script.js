32264450432e9afa194fc92b390a9407ee9ab366
Duban Espinosa <duban18aem@gmail.com>
2024-08-02 18:11:27 -0500

    cambio de funcion estructura html cambiada v1.2

--- boton-flotante-mas/script.js
+++ boton-flotante-mas/script.js
@@ -1,20 +1,15 @@
 const masFloatLabel = document.getElementById("masfloat");
-const checkbox = document.getElementById("btn-mas");
-const btnFloat = document.getElementById("btnfloat");
 
 masFloatLabel.addEventListener("click", () => {
-    checkbox.checked = !checkbox.checked;
-    if (checkbox.checked) {
-        btnFloat.style.display = "flex";
-        setTimeout(() => {
-            btnFloat.style.opacity = "1";
-            btnFloat.style.transform = "translateY(0)";
-        }, 10); // Slight delay to ensure CSS transition
-    } else {
-        btnFloat.style.opacity = "0";
-        btnFloat.style.transform = "translateY(20px)";
-        setTimeout(() => {
-            btnFloat.style.display = "none";
-        }, 300); // Match this duration with the CSS transition time
-    }
+    masFloatLabel.classList.toggle("rotate");
+});
+
+const container = document.getElementById("main-menu");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    container.style.backgroundColor = "#F7F8F900"; /* Cambia el color de fondo cuando se desplaza más de 100px */
+  } else {
+    container.style.backgroundColor = "#F7F8F9"; /* Vuelve al color de fondo predeterminado */
+  }
 });
