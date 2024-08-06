faab8089d7cf7b1b54284003678e261bebc319b1
Duban Espinosa <duban18aem@gmail.com>
2024-08-06 10:48:08 -0500

    cambio de la funcion para mostrar 5 botones ocultos, centrado del boton

--- boton-flotante-mas/script.js
+++ boton-flotante-mas/script.js
@@ -1,15 +1,14 @@
+const btnMas = document.getElementById("btn-mas");
+const btnMasContainer = document.querySelector(".btn-mas");
 const masFloatLabel = document.getElementById("masfloat");
 
-masFloatLabel.addEventListener("click", () => {
-    masFloatLabel.classList.toggle("rotate");
-});
-
-const container = document.getElementById("main-menu");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
-    container.style.backgroundColor = "#F7F8F900"; /* Cambia el color de fondo cuando se desplaza más de 100px */
-  } else {
-    container.style.backgroundColor = "#F7F8F9"; /* Vuelve al color de fondo predeterminado */
-  }
+btnMasContainer.addEventListener("click", () => {
+    btnMas.checked = !btnMas.checked;
+    masFloatLabel.classList.toggle("rotate", btnMas.checked);
+    const btnFloat = document.getElementById("btnfloat");
+    if (btnMas.checked) {
+        btnFloat.style.display = "flex";
+    } else {
+        btnFloat.style.display = "none";
+    }
 });
