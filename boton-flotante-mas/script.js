d04771e7da58a58c2f9eab6e3fd4ca1b4e83a5a4
Duban Espinosa <duban18aem@gmail.com>
2024-08-07 12:26:29 -0500

    Funcion adicional para mejorar el posicionamiento de los botones ocultos y cambio de estilo

--- boton-flotante-mas/script.js
+++ boton-flotante-mas/script.js
@@ -6,9 +6,25 @@ btnMasContainer.addEventListener("click", () => {
     btnMas.checked = !btnMas.checked;
     masFloatLabel.classList.toggle("rotate", btnMas.checked);
     const btnFloat = document.getElementById("btnfloat");
+    const floatButtons = document.querySelectorAll(".flobn");
+
     if (btnMas.checked) {
         btnFloat.style.display = "flex";
+        floatButtons.forEach((button, index) => {
+            setTimeout(() => {
+                button.style.transform = "translateY(0)";
+                button.style.opacity = "1";
+            }, index * 100);
+        });
     } else {
-        btnFloat.style.display = "none";
+        floatButtons.forEach((button, index) => {
+            setTimeout(() => {
+                button.style.transform = "translateY(20px)";
+                button.style.opacity = "0";
+            }, index * 100);
+        });
+        setTimeout(() => {
+            btnFloat.style.display = "none";
+        }, floatButtons.length * 100);
     }
 });
